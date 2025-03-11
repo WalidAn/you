8111f27b7c17035af5b638be62461dd9e2317fc7
Walid Ankour <walid.ankour@ortmontreuil.fr>
2025-03-11 18:21:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('U ARE THE BEST.').split('')
+    const titles = ('i love you Jade.').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
